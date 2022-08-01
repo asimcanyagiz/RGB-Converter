6984c6cbad13dea5a373fa8dad332862f2b720a5
Asim Can Yagiz <meycasim@gmail.com>
2022-08-01 21:35:17 +0300

    Storyboard events added to ViewController

--- RGB-Converter/ViewController.swift
+++ RGB-Converter/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redText: UITextField!
+    @IBOutlet weak var greenText: UITextField!
+    @IBOutlet weak var blueText: UITextField!
+    
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var btnConvert: UIButton!
+    
+    
+    @IBOutlet weak var lblSelectedColor: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func btnConvertClicked(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
